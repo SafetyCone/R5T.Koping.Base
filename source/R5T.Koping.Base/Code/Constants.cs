63566fd46bc238292651ed215e86625480cfe123
MinexAutomation <minex.automation@gmail.com>
2020-02-06 17:53:15 -0800

    Added solution folder project type GUID constant in the base library since it really is a Visual Studio invariant.

---
+++ source/R5T.Koping.Base/Code/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+namespace R5T.Koping.Base
+{
+    public static class Constants
+    {
+        // From: https://www.codeproject.com/Reference/720512/List-of-Visual-Studio-Project-Type-GUIDs
+        // Also from creating a new Visual Studio 2017 solution, adding a solution folder, saving the solution, and examining the file.
+        public static Guid SolutionFolderProjectTypeGuid = Guid.Parse("2150E333-8FDC-42A3-9474-1A3956D46DE8");
+    }
+}
