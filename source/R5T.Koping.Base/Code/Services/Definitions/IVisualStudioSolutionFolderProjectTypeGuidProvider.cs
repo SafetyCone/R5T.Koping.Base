6cbbc691f5007ca2bec0594ce2fb9574f98b2c5b
davidcoats <david@getsafetycone.com>
2022-04-27 15:32:10 -0700

    Interim changes.

--- source/R5T.Koping.Base/Code/Services/Definitions/IVisualStudioSolutionFolderProjectTypeGuidProvider.cs
+++ source/R5T.Koping.Base/Code/Services/Definitions/IVisualStudioSolutionFolderProjectTypeGuidProvider.cs
@@ -1,14 +1,14 @@
-﻿using System;
+using System;using R5T.T0064;
 
 
 namespace R5T.Koping
-{
+{[ServiceDefinitionMarker]
     /// <summary>
     /// Solution files can group projects in solution folders (for example "_Dependencies").
     /// These solution folders are treated as projects by Visual Studio (they have a project reference, just like any other project in the solution file), but with the particular property of being able to have nested projects mapped to them.
     /// This service provides the project type GUID value that indicates a project is a solution folder.
     /// </summary>
-    public interface IVisualStudioSolutionFolderProjectTypeGuidProvider
+    public interface IVisualStudioSolutionFolderProjectTypeGuidProvider:IServiceDefinition
     {
         Guid GetVisualStudioSolutionFolderProjectTypeGuid();
     }
